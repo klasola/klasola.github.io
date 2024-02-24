8e3f2f837c514ffa111f579f67bcda415f95c3f2
klasola <kalleylaso@gmail.com>
2024-02-23 23:36:55 -0500

    pokeprice deploy

---
+++ src/js/pokeprice.js
@@ -0,0 +1,121 @@
+// PURPOSE: Pokeprice functionality
+// AUTHOR: Kalley Lasola
+
+const controller = {};
+const MAX_POKEMON = 1025;
+controller.p1_weight = 0;
+controller.p2_weight = 0;
+controller.state = "";
+controller.streak = 0;
+controller.highestStreak = 0;
+
+controller.getRandNum = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+controller.getPokemon = async function(pokemonId) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  const data = await response.json();
+  return data;
+}
+
+controller.getSprite = function(pokemon) {
+  return pokemon.sprites.front_default;
+}
+
+controller.generatePokemon = async function() {
+  // Populate first pokemon
+  let p1 = await controller.getPokemon(controller.getRandNum(1, MAX_POKEMON));
+  let p1_name = p1.name.charAt(0).toUpperCase() + p1.name.slice(1);
+  $("#pokemon1 .p-name").html(p1_name);
+  $("#q-name-1").html(p1_name);
+  $("#pokemon1 img").attr("src", controller.getSprite(p1));
+  controller.p1_weight = Math.round(p1.weight / 4.536);
+
+  // Populate second pokemon
+  let p2 = await controller.getPokemon(controller.getRandNum(1, MAX_POKEMON));
+  let p2_name = p2.name.charAt(0).toUpperCase() + p2.name.slice(1);
+  $("#pokemon2 .p-name").html(p2_name);
+  $("#q-name-2").html(p2_name);
+  $("#pokemon2 img").attr("src", controller.getSprite(p2));
+  controller.p2_weight = Math.round(p2.weight / 4.536);
+}
+
+controller.updateStreaks = function() {
+  $('#streak').html(controller.streak);
+  $('#highest-streak').html(controller.highestStreak);
+}
+
+controller.showWeights = function() {
+  $("#pokemon1 .p-weight").html(controller.p1_weight + " lbs");
+  $("#pokemon2 .p-weight").html(controller.p2_weight + " lbs");
+}
+
+controller.hideWeights = function() {
+  $("#pokemon1 .p-weight").html("");
+  $("#pokemon2 .p-weight").html("");
+}
+
+controller.refresh = function() {
+  $("#message").html("");
+  controller.hideWeights();
+  controller.generatePokemon();
+  setTimeout(function() {
+    $("#q-content").show();
+  }, 500);
+}
+
+controller.updateDisplay = function() {
+  if (controller.state) {
+    // Correct
+    controller.streak = controller.streak + 1;
+    if (controller.streak > controller.highestStreak) {
+      controller.highestStreak = controller.streak;
+    }
+    controller.updateStreaks();
+    $("#q-content").hide();
+    $("#message").html("Correct!");
+    controller.showWeights();
+    setTimeout(function() {
+      controller.refresh();
+    }, 3000);
+  } else {
+    // Incorrect
+    controller.streak = 0;
+    controller.updateStreaks();
+    $("#q-content").hide();
+    $("#message").html("Wrong!");
+    controller.showWeights();
+    setTimeout(function() {
+      controller.refresh();
+    }, 3000);
+  }
+}
+
+controller.setListeners = function() {
+  $("#fatter-button").click(function() {
+    if (controller.p1_weight > controller.p2_weight) {
+      controller.state = true;
+    } else {
+      controller.state = false;
+    }
+    controller.updateDisplay();
+  });
+
+  $("#skinnier-button").click(function() {
+    if (controller.p1_weight < controller.p2_weight) {
+      controller.state = true;
+    } else {
+      controller.state = false;
+    }
+    controller.updateDisplay();
+  });
+}
+
+$( document ).ready(async function() {
+  controller.generatePokemon();
+  controller.setListeners();
+  controller.updateStreaks();
+});
